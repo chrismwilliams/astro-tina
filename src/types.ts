4b194a69da373fb3e48d0f60605c4b1dc898d093
Chris Williams <chrismwilliams@users.noreply.github.com>
2024-05-28 16:08:35 +0100

    add mdx + frontmatter type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import type { MDXInstance } from "astro";
+
 export interface SiteConfig {
 	author: string;
 	date: {
@@ -9,10 +11,6 @@ export interface SiteConfig {
 	ogLocale: string;
 	sortPostsByUpdatedDate: boolean;
 	title: string;
-	webmentions?: {
-		link: string;
-		pingback?: string;
-	};
 }
 
 export interface PaginationLink {
@@ -28,58 +26,14 @@ export interface SiteMeta {
 	title: string;
 }
 
-/** Webmentions */
-export interface WebmentionsFeed {
-	children: WebmentionsChildren[];
-	name: string;
-	type: string;
+export interface PostFrontmatter {
+	description: string;
+	draft: boolean;
+	minutesRead: string;
+	publishedDate: string;
+	slug: string;
+	title: string;
+	updatedDate?: string;
 }
 
-export interface WebmentionsCache {
-	children: WebmentionsChildren[];
-	lastFetched: null | string;
-}
-
-export interface WebmentionsChildren {
-	author: Author | null;
-	content?: Content | null;
-	"mention-of": string;
-	name?: null | string;
-	photo?: null | string[];
-	published?: null | string;
-	rels?: Rels | null;
-	summary?: Summary | null;
-	syndication?: null | string[];
-	type: string;
-	url: string;
-	"wm-id": number;
-	"wm-private": boolean;
-	"wm-property": string;
-	"wm-protocol": string;
-	"wm-received": string;
-	"wm-source": string;
-	"wm-target": string;
-}
-
-export interface Author {
-	name: string;
-	photo: string;
-	type: string;
-	url: string;
-}
-
-export interface Content {
-	"content-type": string;
-	html: string;
-	text: string;
-	value: string;
-}
-
-export interface Rels {
-	canonical: string;
-}
-
-export interface Summary {
-	"content-type": string;
-	value: string;
-}
+export type MDXPost = MDXInstance<PostFrontmatter>;
