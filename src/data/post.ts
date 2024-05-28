4bd811286e5c9354d8004070b47959f12d3e2e7a
Chris Williams <chrismwilliams@users.noreply.github.com>
2024-05-28 16:08:51 +0100

    update post queries

--- src/data/post.ts
+++ src/data/post.ts
@@ -1,22 +1,16 @@
-import { siteConfig } from "@/site-config";
-import { type CollectionEntry, getCollection } from "astro:content";
+import type { MDXPost } from "@/types";
 
-/** filter out draft posts based on the environment */
-export async function getAllPosts() {
-	return await getCollection("post", ({ data }) => {
-		return import.meta.env.PROD ? !data.draft : true;
-	});
-}
+import { siteConfig } from "@/site-config";
 
 /** returns the date of the post based on option in siteConfig.sortPostsByUpdatedDate */
-export function getPostSortDate(post: CollectionEntry<"post">) {
-	return siteConfig.sortPostsByUpdatedDate && post.data.updatedDate !== undefined
-		? new Date(post.data.updatedDate)
-		: new Date(post.data.publishDate);
+export function getPostSortDate(post: MDXPost) {
+	return siteConfig.sortPostsByUpdatedDate && post.frontmatter.updatedDate !== undefined
+		? new Date(post.frontmatter.updatedDate)
+		: new Date(post.frontmatter.publishedDate);
 }
 
 /** sort post by date (by siteConfig.sortPostsByUpdatedDate), desc.*/
-export function sortMDByDate(posts: CollectionEntry<"post">[]) {
+export function sortMDByDate(posts: MDXPost[]) {
 	return posts.sort((a, b) => {
 		const aDate = getPostSortDate(a).valueOf();
 		const bDate = getPostSortDate(b).valueOf();
@@ -27,8 +21,8 @@ export function sortMDByDate(posts: CollectionEntry<"post">[]) {
 /** groups posts by year (based on option siteConfig.sortPostsByUpdatedDate), using the year as the key
  *  Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so.
  */
-export function groupPostsByYear(posts: CollectionEntry<"post">[]) {
-	return posts.reduce<Record<string, CollectionEntry<"post">[]>>((acc, post) => {
+export function groupPostsByYear(posts: MDXPost[]) {
+	return posts.reduce<Record<string, MDXPost[]>>((acc, post) => {
 		const year = getPostSortDate(post).getFullYear();
 		if (!acc[year]) {
 			acc[year] = [];
